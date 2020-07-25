b8c97dde15075edb9b7b6107586bd3176d140f2d
Edwin Eefting <edwin@datux.nl>
2020-07-25 10:13:35 +0200

    bla

--- main.cpp
+++ main.cpp
@@ -13,13 +13,16 @@ vector< shared_ptr< Session > > sessions;
 
 void register_event_source_handler( const shared_ptr< Session > session )
 {
+	static long id = 0;
+    id++;
+
 	const auto headers = multimap< string, string > {
 		{ "Connection", "keep-alive" },
 		{ "Cache-Control", "no-cache" },
 		{ "Content-Type", "text/event-stream" },
 		{ "Access-Control-Allow-Origin", "*" } //Only required for demo purposes.
 	};
-
+    session->set("blaat", id);
 
 	session->yield( OK, headers, [ ]( const shared_ptr< Session > session )
 	{
@@ -30,17 +33,18 @@ void register_event_source_handler( const shared_ptr< Session > session )
 void event_stream_handler( void )
 {
 	static size_t counter = 0;
-	const auto message = "data: event " + to_string( counter ) + "\n\n";
+	// const auto message = "data: event " + to_string( counter ) + "\n\n";
 
-	// sessions.erase(
-	// 	  std::remove_if(sessions.begin(), sessions.end(),
-    //                             [](const shared_ptr<Session> &a) {
-    //                               return a->is_closed();
-    //                             }),
-	// 	  sessions.end());
+	sessions.erase(
+		  std::remove_if(sessions.begin(), sessions.end(),
+                                [](const shared_ptr<Session> &a) {
+                                  return a->is_closed();
+                                }),
+		  sessions.end());
 
 	for ( auto session : sessions )
 	{
+        auto message = "data: event " + to_string((long)session->get("blaat"))  + "\n\n";
 		session->yield( message );
 	}
 
@@ -55,7 +59,7 @@ int main( const int, const char** )
     
     auto settings = make_shared< Settings >( );
     settings->set_port( 1984 );
-    settings->set_connection_limit(10);
+    settings->set_worker_limit(10);
     
     auto service = make_shared< Service >( );
     service->publish( resource );

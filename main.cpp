4c28ae11498ac3db573763c15bf19fc6700376c7
Edwin Eefting <edwin@datux.nl>
2020-08-02 21:54:48 +0200

    threading

--- main.cpp
+++ main.cpp
@@ -44,6 +44,7 @@
 #include "msg_session_manager.hpp"
 #include "log.hpp"
 #include "filecache.hpp"
+#include <thread>
 
 #include <App.h>
 
@@ -53,82 +54,10 @@ using namespace std;
 // using namespace restbed;
 using namespace std::chrono;
 
-// vector<shared_ptr<Session>> sessions;
 
-// void yield_text(Session &session, const int status = 200, const string &text = "", const std::function<void(std::shared_ptr<restbed::Session>)> &callback = nullptr)
-// {
-// 	const multimap<string, string> headers{
-// 		{"Connection", "keep-alive"},
-// 		{"Content-Length", ::to_string(text.length())}};
-
-// 	session.yield(status, text, headers, callback);
-// }
-
-// //handle static content
-// void handle_static(const shared_ptr<Session> session)
-// {
-// 	const auto request = session->get_request();
-// 	const string filename = request->get_path_parameter("filename");
-
-// 	ifstream stream("../wwwdir/" + filename, ifstream::in);
-
-// 	if (!stream.is_open())
-// 	{
-// 		yield_text(*session, NOT_FOUND, "File not found.\n");
-// 	}
-// 	else
-// 	{
-// 		boost::smatch what;
-// 		if (!regex_search(
-// 				filename,
-// 				what,
-// 				boost::regex("([^.]*$)")))
-// 		{
-// 			yield_text(*session, INTERNAL_SERVER_ERROR, "Incorrect filename format\n");
-// 		}
-// 		else
-// 		{
-// 			//send file
-// 			string extention = what[1];
-// 			string content_type = content_type_map.at(extention);
-// 			const string body = string(istreambuf_iterator<char>(stream), istreambuf_iterator<char>());
-
-// 			const multimap<string, string> headers{
-// 				{"Content-Type", content_type},
-// 				{"Connection", "keep-alive"},
-// 				{"Content-Length", ::to_string(body.length())}};
-
-// 			session->yield(OK, body, headers);
-// 		}
-// 	}
-// }
 
 // MsgSessionManager msg_session_manager;
 
-// //register new server side event session
-// void handle_events(const shared_ptr<Session> session)
-// {
-// 	static long id = 0;
-// 	id++;
-
-// 	const auto headers = multimap<string, string>{
-// 		{"Connection", "keep-alive"},
-// 		{"Cache-Control", "no-cache"},
-// 		{"Content-Type", "text/event-stream"},
-// 		{"Access-Control-Allow-Origin", "*"} //Only required for demo purposes.
-// 	};
-
-// 	session->yield(OK, headers, [](const shared_ptr<Session> session) {
-// 		//this is also what keeps the session alive
-// 		auto msg_session = msg_session_manager.create(session);
-
-// 		string message;
-// 		message += "data: event ";
-// 		message += msg_session->m_id;
-// 		message += "\n\n";
-// 		session->yield(message);
-// 	});
-// }
 
 // //handle incomming message via /send
 // void handle_send(const shared_ptr<Session> session)
@@ -172,109 +101,48 @@ FileCacher file_cacher("../wwwdir");
 int main(const int, const char **)
 {
 
+
+
+	std::vector<std::thread *> threads(std::thread::hardware_concurrency());
+
+	std::transform(threads.begin(), threads.end(), threads.begin(), [](std::thread *t) {
+		return new std::thread([]() {
+
 #ifdef SSL
-	uWS::SSLApp({.key_file_name = "../misc/key.pem",
-				 .cert_file_name = "../misc/cert.pem",
-				 .passphrase = "1234"})
+			uWS::SSLApp({.key_file_name = "../misc/key.pem",
+						 .cert_file_name = "../misc/cert.pem",
+						 .passphrase = "1234"})
 #else
-	uWS::App()
+																				  uWS::App()
 #endif
-		.get("/*", [](auto *res, auto *req) {
-			// string s;
+				.get("/*", [](auto *res, auto *req) {
+					auto file = file_cacher.get(string(req->getUrl()));
 
-			// string_view view();
-			auto  file=file_cacher.get(string(req->getUrl()));
-			
+					if (file == file_cacher.m_cached_files.end())
+					{
+						res->writeStatus("404");
+						res->end("not found");
+					}
+					else
+						res->end(file->second.m_view);
 
-			if (file==file_cacher.m_cached_files.end())
-			{
-				res->writeStatus("404");
-				res->end("not found");
-			}
-			else
-				res->end(file->second.m_view);
+					return;
+				})
+				.listen(3000, [](auto *token) {
+					if (token)
+					{
 
+						INFO("Listening on port " << 3000 << std::endl)
+					}
+				})
+				.run();
+		});
+	});
 
+	std::for_each(threads.begin(), threads.end(), [](std::thread *t) {
+		t->join();
+	});
 
-			// res->onWritable([s,res](int offset) {
-			// // res->cork([s,res,offset] {
-
-			// 	   ERROR("WIL MEER!" << offset)
-			// 		res->tryEnd(s, 100000000);
-			// 	   ERROR("MEER gegeve!" << offset)
-			// // });
-			// 	   return true;
-
-
-			//    })
-			// 	->onAborted([]() {
-			// 		ERROR("ABORTED!");
-			// 	});
-				
-			// ERROR("STARTS");
-			// ERROR("START");
-			// res->tryEnd(s, 100000000);
-			// ERROR("done");
-
-
-			// res->cork([res] {
-			// 	while (true)
-			// 	{
-			// 		std::pair<bool, bool> status;
-			// 		status = res->tryEnd("Hello worl", 1000000);
-			// 		if (!status.first) 
-			// 		{
-			// 			ERROR("NOT OK BREAK");
-			// 			break;
-			// 		}
-			// 		if (status.second) 
-			// 		{
-			// 			ERROR("RESPONDED BREAK");
-			// 			break;
-			// 		}
-			// 		ERROR("SKRIEM")
-			// 	}
-			// });
-			return;
-		})
-		.listen(3000, [](auto *token) {
-			if (token)
-			{
-
-				INFO("Listening on port " << 3000 << std::endl)
-			}
-		})
-		.run();
-
-	// auto service = make_shared<Service>();
-
-	// {
-	// 	auto resource = make_shared<Resource>();
-	// 	resource->set_path("/static/{filename: [a-zA-Z0-9._-]*}");
-	// 	resource->set_method_handler("GET", handle_static);
-	// 	service->publish(resource);
-	// }
-	// {
-	// 	auto resource = make_shared<Resource>();
-	// 	resource->set_path("/events");
-	// 	resource->set_method_handler("GET", handle_events);
-	// 	service->publish(resource);
-	// }
-	// {
-	// 	auto resource = make_shared<Resource>();
-	// 	resource->set_path("/send");
-	// 	resource->set_method_handler("POST", handle_send);
-	// 	service->publish(resource);
-	// }
-
-	// service->schedule(send_events, milliseconds(1000));
-	// // service->set_session_manager( make_shared< InMemorySessionManager >( ) );
-
-	// auto settings = make_shared<Settings>();
-	// settings->set_port(1984);
-	// settings->set_worker_limit(10);
-	// settings->set_connection_limit(1000);
-	// service->start(settings);
 
 	return EXIT_SUCCESS;
 }

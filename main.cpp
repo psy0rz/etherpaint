7e86c9aeafb132942c4f89662d2d95f577a2679a
Edwin Eefting <edwin@datux.nl>
2020-08-07 13:01:25 +0200

    bla

--- main.cpp
+++ main.cpp
@@ -1,52 +1,57 @@
 /* Released under GNU General Public License v3.0
- * (C) Edwin Eefting -- ediwn@datux.nl 
+ * (C) Edwin Eefting -- ediwn@datux.nl
  *
- * This framework is designed with performance and scalability in mind. All design decisions are made accordingly:
- *  - I choose restbed because its superfast, clean, stable and has server-side event support. (and is event driven and multi threaded)
- *  - I choose rapidjson because its the fastest AND it has a nice DOM API. (and stable and secure as well, its created by the desginers of Weechat i think, it has regression testing and 100% testing coverage)
- *  - We dont use regular cookies (for performance reasons, we send up to 25 POSTs per second)
- *  - We dont want extra headers, so we stay compatible with proxies.
- *  - Note that a "session" here is an active message-stream from the server to a javascript instance in the browser. Not to be confused with regular php session stuff.
- * 
+ * This framework is designed with performance and scalability in mind. All
+ * design decisions are made accordingly:
+ *  - I choose uwebsockets because its the fastest out here. (first did some testing with restbed which is awesome as well)
+ *  - I choose rapidjson because its the fastest AND it has a nice DOM API. (and
+ * stable and secure as well, its created by the desginers of Weechat i think,
+ * it has regression testing and 100% testing coverage)
+ *  - We dont use regular cookies (for performance reasons, we send up to 25
+ * POSTs per second)
+ *  - Note that a "session" here is an active websocket from the server to
+ * a javascript instance in the browser. Not to be confused with regular php
+ * session stuff.
+ *
  * The external message format (json) between browser and server:
- *  - Every message contains the session id and a bunch of events. (events are batched for performance reasons)
- *  - Data can be any json compatible structure.
  *  - Messages from/to server have the same format:
- * 
- *    [ "sessionid", [ [ "eventname", data ], [ ... ] ] ]
  *
- *  - Messages FROM server to brower are sent via server-sent events via the url /events:
- *  - As soon as this connection dies, the session becomes invalid and all is lost. :)  (javascript stuff will nicely reconnect/resync)
+ *    [ "eventname", data ]
+ *  - Data can be any json compatible structure.
+  *
+ *  - As soon as this connection dies, the session becomes invalid and all is
+ * lost. :)  (javascript stuff will nicely reconnect/resync)
  *
  * The internal message format (C++):
- *  - For performance reasons we stay as close to rapidjson as possible, so we dont have to create extra copies or datastructures. 
+ *  - For performance reasons we stay as close to rapidjson as possible, so we
+ * dont have to create extra copies or datastructures.
  *  - EventHandlers are called with references to the rapidjson document object.
- *  - Only after execution of all eventhandlers the object will be discarded and an OK will be yielded.
- * 
- * 
- * 
+ *  - Only after execution of all eventhandlers the object will be discarded.
+ *
+ *
+ *
  */
 
-#include <map>
 #include <chrono>
-#include <string>
-#include <memory>
 #include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
 // #include <restbed>
-#include <iostream>
-#include <fstream>
-#include "rapidjson/document.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/error/en.h"
+#include <App.h>
 
 #include <boost/regex.hpp>
-#include "msg_session_manager.hpp"
-#include "log.hpp"
-#include "filecache.hpp"
+#include <fstream>
+#include <iostream>
 #include <thread>
 
-#include <App.h>
+#include "filecache.hpp"
+#include "log.hpp"
+#include "msg_session_manager.hpp"
+#include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 using namespace rapidjson;
 
@@ -54,10 +59,7 @@ using namespace std;
 // using namespace restbed;
 using namespace std::chrono;
 
-
-
 // MsgSessionManager msg_session_manager;
-
 
 // //handle incomming message via /send
 // void handle_send(const shared_ptr<Session> session)
@@ -66,19 +68,25 @@ using namespace std::chrono;
 
 // 	size_t content_length = request->get_header("Content-Length", 0);
 
-// 	session->fetch(content_length, [request](const shared_ptr<Session> session, const Bytes &body) {
+// 	session->fetch(content_length, [request](const shared_ptr<Session>
+// session, const Bytes &body) {
 // 		// fprintf(stdout, "RAW %.*s\n", (int)body.size(), body.data());
 
 // 		auto document = make_shared<Document>();
-// 		//note: can perhaps prevent a copy by using document->ParseInsitu
-// 		document->Parse(reinterpret_cast<const char *>(body.data()), body.size());
+// 		//note: can perhaps prevent a copy by using
+// document->ParseInsitu 		document->Parse(reinterpret_cast<const
+// char
+// *>(body.data()), body.size());
 
 // 		if (document->HasParseError())
 // 		{
 // 			stringstream error_text;
-// 			error_text << "Parse error at offset " << document->GetErrorOffset() << ": " << GetParseError_En(document->GetParseError()) << "\n";
+// 			error_text << "Parse error at offset " <<
+// document->GetErrorOffset() << ": " <<
+// GetParseError_En(document->GetParseError()) << "\n";
 
-// 			yield_text(*session, INTERNAL_SERVER_ERROR, error_text.str());
+// 			yield_text(*session, INTERNAL_SERVER_ERROR,
+// error_text.str());
 // 		}
 // 		else
 // 		{
@@ -98,51 +106,97 @@ using namespace uWS;
 
 FileCacher file_cacher("../wwwdir");
 
-int main(const int, const char **)
+struct PerSocketData
+{};
+
+int
+main(const int, const char**)
 {
+  std::vector<std::thread*> threads(std::thread::hardware_concurrency());
 
-
-
-	std::vector<std::thread *> threads(std::thread::hardware_concurrency());
-
-	std::transform(threads.begin(), threads.end(), threads.begin(), [](std::thread *t) {
-		return new std::thread([]() {
+  std::transform(
+    threads.begin(), threads.end(), threads.begin(), [](std::thread* t) {
+      return new std::thread([]() {
 
 #ifdef SSL
-			uWS::SSLApp({.key_file_name = "../misc/key.pem",
-						 .cert_file_name = "../misc/cert.pem",
-						 .passphrase = "1234"})
+        uWS::SSLApp({ .key_file_name = "../misc/key.pem",
+                      .cert_file_name = "../misc/cert.pem",
+                      .passphrase = "1234" })
 #else
-																				  uWS::App()
+		    uWS::App()
 #endif
-				.get("/*", [](auto *res, auto *req) {
-					auto file = file_cacher.get(string(req->getUrl()));
+          .get("/*",
+               [](auto* res, auto* req) {
+                 auto file = file_cacher.get(string(req->getUrl()));
 
-					if (file == file_cacher.m_cached_files.end())
-					{
-						res->writeStatus("404");
-						res->end("not found");
-					}
-					else
-						res->end(file->second.m_view);
+                 if (file == file_cacher.m_cached_files.end()) {
+                   res->writeStatus("404");
+                   res->end("not found");
+                 } else
+                   res->end(file->second.m_view);
 
-					return;
-				})
-				.listen(3000, [](auto *token) {
-					if (token)
-					{
+                 return;
+               })
+          .ws<PerSocketData>(
+            "/ws",
+            { .compression = uWS::DISABLED,
+              .maxPayloadLength = 16 * 1024,
+              .idleTimeout = 1000,
+              .maxBackpressure = 1024,
+              /* Handlers */
+              .open = [](auto* ws) { DEB("websocket open" << ws); },
+              .message =
+                [](auto* ws, std::string_view message, uWS::OpCode opCode) {
+                  auto document = make_shared<Document>();
+                  // note: can perhaps prevent a copy by using
+                  // document->ParseInsitu
+                  document->Parse(reinterpret_cast<const char*>(message.data()),
+                                  message.size());
+                  if (document->HasParseError()) {
+                    stringstream error_text;
+                    error_text << "Parse error at offset "
+                               << document->GetErrorOffset() << ": "
+                               << GetParseError_En(document->GetParseError());
+                    ERROR(error_text.str());
+                    // yield_text(*session, INTERNAL_SERVER_ERROR,
+                    // error_text.str());
+                  } else {
+                    
+                    // convert test
+                    // StringBuffer buffer;
+                    // Writer<StringBuffer> writer(buffer);
+                    // document->Accept(writer);
+                    // DEB("stringified " << buffer.GetString());
 
-						INFO("Listening on port " << 3000 << std::endl)
-					}
-				})
-				.run();
-		});
-	});
+                    // /			yield_text(*session, OK);
+                    DEB("chus");
+                  }
+                },
+              //    ws->send(message, opCode);
+              //    DEB("websocket message" << ws); },
+              .drain =
+                [](auto* ws) {
+                  /* Check getBufferedAmount here */
+                },
+              .ping = [](auto* ws) { DEB("websocket ping" << ws); },
+              .pong = [](auto* ws) { DEB("websocket pong" << ws); },
+              .close =
+                [](auto* ws, int code, std::string_view message) {
+                  DEB("websocket close" << ws);
+                } })
 
-	std::for_each(threads.begin(), threads.end(), [](std::thread *t) {
-		t->join();
-	});
+          .listen(3000,
+                  [](auto* token) {
+                    if (token) {
+                      INFO("Listening on port " << 3000 << std::endl)
+                    }
+                  })
+          .run();
+      });
+    });
 
+  std::for_each(
+    threads.begin(), threads.end(), [](std::thread* t) { t->join(); });
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }

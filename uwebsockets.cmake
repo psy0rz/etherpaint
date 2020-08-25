f4e30b851962168eb3d0b493da57e714619bccd9
Edwin Eefting <edwin@datux.nl>
2020-08-25 18:26:49 +0200

    moved uwebsockets to externalproject

---
+++ uwebsockets.cmake
@@ -0,0 +1,18 @@
+ExternalProject_Add(
+        uwebsockets
+        PREFIX "vendor/uwebsockets"
+        GIT_REPOSITORY "https://github.com/uNetworking/uWebSockets.git"
+        GIT_TAG v18.11.0
+        GIT_SUBMODULES_RECURSE On
+        TIMEOUT 10
+        CONFIGURE_COMMAND "" #leave commented so cmake uses the cmakelists.txt of this external project.
+        BUILD_COMMAND WITH_OPENSSL=1 make -j1 -C <SOURCE_DIR>/uSockets
+        INSTALL_COMMAND ""
+        UPDATE_COMMAND ""
+)
+
+ExternalProject_Get_Property(uwebsockets SOURCE_DIR)
+set(UWEBSOCKETS_INCLUDE_DIRS ${SOURCE_DIR}/src ${SOURCE_DIR}/uSockets/src)
+set(LIBUSOCKETS ${SOURCE_DIR}/uSockets/uSockets.a)
+
+

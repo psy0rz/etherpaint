5a212830e5d6a1d517728bd4eb30e6ca878f7c35
Edwin Eefting <edwin@datux.nl>
2020-09-02 14:16:57 +0200

    store cursors

--- apps/paper/MsgSessionPaper.h
+++ apps/paper/MsgSessionPaper.h
@@ -5,10 +5,18 @@
 #ifndef PAPER_MSGSESSIONPAPER_H
 #define PAPER_MSGSESSIONPAPER_H
 #include "messages/MsgSession.h"
+#include "messages_generated.h"
+
 
 class MsgSessionPaper:public MsgSession {
 public:
     explicit MsgSessionPaper(uWS::WebSocket<ENABLE_SSL, true> *ws);
+
+    struct {
+        uint16_t x;
+        uint16_t y;
+    } cursor;
+
 
 
 };
--- apps/paper/main.cpp
+++ apps/paper/main.cpp
@@ -1,6 +1,7 @@
 #include "messages/messages.h"
 #include "messages/register_handler.h"
 #include "SharedSessionPaper.h"
+#include "MsgSessionPaper.h"
 
 
 int main(const int argc, const char *argv[]) {
@@ -17,9 +18,14 @@ int main(const int argc, const char *argv[]) {
 
     handlers[event::EventUnion_Cursor] = [](const std::shared_ptr<MsgSession> &msg_session, const msg_type &msg,
                                             auto event_index) {
-        auto event = msg->events()->GetAs<event::Cursor>(event_index);
+        const auto & event = msg->events()->GetAs<event::Cursor>(event_index);
+        const auto & msg_session_paper= std::static_pointer_cast<MsgSessionPaper>(msg_session);
 
-        INFO("cursor " << event->x() << "," << event->y());
+        msg_session_paper->cursor.x=event->x();
+        msg_session_paper->cursor.y=event->y();
+
+//        INFO("cursor " << event->x() << "," << event->y());
+
     };
 
     std::thread update_thread(SharedSessionPaper::update_thread);

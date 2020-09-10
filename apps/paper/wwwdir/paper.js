9d4c211f4e269e725b9a2b5b58a99bf54d02d280
Edwin Eefting <edwin@datux.nl>
2020-09-10 16:45:02 +0200

    holyshit its fast! 10000 draw increments are almost instat (<100ms)

--- apps/paper/wwwdir/paper.js
+++ apps/paper/wwwdir/paper.js
@@ -15,7 +15,7 @@ paper.start = function (viewer_element, paper_element, scratch_element) {
     paper.paper_svg = SVG(paper_element);
     paper.scratch_svg = SVG(scratch_element);
 
-    paper.paper_svg.text("Work in progress - het is stuk :)").attr('font-size', '200%');
+    paper.paper_svg.text("Alleen pencil en history slider werken. ").attr('font-size', '200%');
 
 
     paper.clear();

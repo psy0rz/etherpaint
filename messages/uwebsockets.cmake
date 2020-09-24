884660f8f26aac62073caaffba7362eb23c81418
Edwin Eefting <edwin@datux.nl>
2020-09-24 12:03:29 +0200

    buildfixes

--- messages/uwebsockets.cmake
+++ messages/uwebsockets.cmake
@@ -3,12 +3,14 @@ ExternalProject_Add(
         PREFIX "vendor/uwebsockets"
         GIT_REPOSITORY "https://github.com/uNetworking/uWebSockets.git"
         GIT_TAG v18.11.0
-        GIT_SUBMODULES_RECURSE On
+        #leave off, gives weird error and it recurses without this option as well:
+        #GIT_SUBMODULES_RECURSE On
         TIMEOUT 10
         CONFIGURE_COMMAND "" #leave commented so cmake uses the cmakelists.txt of this external project.
         BUILD_COMMAND WITH_OPENSSL=1 CC=${CMAKE_C_COMPILER} make -j1 -C <SOURCE_DIR>/uSockets
         INSTALL_COMMAND ""
         UPDATE_COMMAND ""
+        WORKING_DIRECTORY "GEERT${EXTERNAL_PROJECTS_PREFIX}"
 )
 
 ExternalProject_Get_Property(uwebsockets SOURCE_DIR)
